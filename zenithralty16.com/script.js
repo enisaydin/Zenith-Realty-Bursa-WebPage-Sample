cf2a46c96e2dfe798b9c7f9ee1bba32eeabc72e9
enisaydin <enisaydineng@gmail.com>
2024-11-14 06:32:07 +0300

    Zenith Realty Web Page Sample
    Zenith Realty Web Page Sample

---
+++ zenithralty16.com/script.js
@@ -0,0 +1,111 @@
+const translations = {
+  tr: {
+    "nav-home": "Anasayfa",
+    "nav-about": "Hakkımızda",
+    "nav-why": "Neden Biz?",
+    "nav-contact": "İletişim",
+    "about-title": "Hakkımızda",
+    "nav-team": "Ekibimiz",
+    "nav-explore": "İlanlarımız",
+    "carousel1-h2": "ZAMAN KAZANIN" ,
+    "carousel1-p" :  "Bizimle birlikte hayallerinizdeki eve hızlı ve profesyonel bir süreçle ulaşın.",
+    "carousel2-h2": "AKLINIZ KALMASIN" ,
+    "carousel2-p" : "Uzman ekibimiz aklınıza takılan her konuda kaliteli çözümler üretmek için sizlerle.",
+    "carousel3-h2": "FIRSATLARI KAÇIRMAYIN" ,
+    "carousel3-p" : "Geniş ve çeşitli portföyümüz sayesinde kendinize uygun olan fırsatlara rahatça ulaşın.",
+    "about-content":
+      "Zenith Realty olarak, Bursa’nın gayrimenkul sektöründe uzun yıllara dayanan tecrübemizle güvenilir ve profesyonel danışmanlık hizmeti sunmaktayız. Misyonumuz, müşterilerimizin gayrimenkul ihtiyaçlarını en yüksek memnuniyet seviyesinde karşılamak, vizyonumuz ise sektörde yenilikçi, güvenilir ve sürdürülebilir çözümler sunarak lider bir marka olmaktır. Bursa’daki uzman ekibimiz, her biri kendi alanında profesyonel  danışmanlarımızla sizlere hizmet sunarken, dürüstlük ve şeffaflık ilkelerini esas alır. Yılların birikimi ile edindiğimiz deneyim sayesinde, gayrimenkul sektörünün tüm inceliklerini ve gelişmelerini  yakından takip ediyor, bu doğrultuda sizlere en güncel ve etkiliçözümleri sunuyoruz. Zenith Realty, gayrimenkul yatırımlarınızda  yalnızca aracılık hizmeti değil, aynı zamanda güvenilir bir yol arkadaşı  olmayı hedefler. Müşteri odaklı hizmet anlayışımız ile sizin çıkarlarınızı koruyarak, gayrimenkul işlemlerinizin her aşamasında yanınızda olmaktan gurur duyuyoruz. Geniş portföyümüz, titizlikle değerlendirilmiş mülkler ve kaliteli çözümler sunma hedefimizle Bursa’dalider bir gayrimenkul danışmanlığı hizmeti sunuyoruz. İster yatırım yapmayı düşünün, ister yaşamak için bir ev arayın; Zenith Realty, sizin için en doğru tercihleri sunarak güvenli ve memnuniyet odaklı bir hizmet sağlamaktadır.",
+    "why-title": "Neden Bizi Tercih Etmelisiniz?",
+    "why-reliability": "Güvenilirlik",
+    "why-reliability-content":
+      "Uzman ekibimizle yılların deneyimiyle güvenilir hizmet sunuyoruz.",
+    "why-quality": "Kalite",
+    "why-quality-content":
+      "Her zaman en kaliteli çözümleri sunmaya odaklanıyoruz.",
+    "why-satisfaction": "Müşteri Memnuniyeti",
+    "why-satisfaction-content":
+      "Müşterilerimizin memnuniyeti bizim için her şeyden önemli.",
+    "team-title": "Ekibimiz",
+    "team-member1-name": "Halil İbrahim AYBEY",
+    "team-member1-role": "Gayrimenkul Danışmanı",
+    "team-member2-name": "Hediye ÇELİK",
+    "team-member2-role": "Broker",
+    "team-member3-name": "Soner AYDIN",
+    "team-member3-role":"Gayrimenkul Danışmanı",
+    "explore-treng": "Tüm ilanlarımızı inceleyin ve kaçırmak istemeyeceğiniz fırsatlara göz atın.",
+    "exploretext" : "İlanlara Göz At",
+    "contact-subtitle1": "Bize Ulaşın",
+    "contact-title": "İletişim",
+    "contact-name": "Adınız",
+    "contact-email": "Email",
+    "contact-message": "Mesajınız",
+    "contact-submit": "Gönder",
+    "contact-address-title": "Adres Bilgileri",
+    "copyr": "&copy; 2024 Ajans FLU. Tüm Hakları Saklıdır.",
+    "contact-address-content":
+      "Zenith Realty<br />Tepkar İş Merkezi Alaattinbey Mah.<br /> 626 Sk. No:12 Kat:2 Kapı No:7 <br />Nilüfer, Bursa<br />Telefon: (0212) 123 45 67<br />Email: zenithrealty16@gmail.com",
+  },
+  en: {
+    "nav-home": "Home",
+    "nav-about": "About Us",
+    "nav-why": "Why Us?",
+    "nav-contact": "Contact",
+    "about-title": "About Us",
+    "nav-team": "Team",
+    "nav-explore": "Portfolio",
+
+
+    "carousel1-h2": "DO NOT WASTE TIME" ,
+    "carousel1-p" : "Reach your dream home with us through a fast and professional process.",
+    "carousel2-h2": "HAVE NO REGRETS" ,
+    "carousel2-p" : "Our expert team is here to provide quality solutions for any questions you may have.",
+    "carousel3-h2": "DO NOT MISS THE OPPORTUNITIES" ,
+    "carousel3-p" : "With our wide and diverse portfolio, easily find the opportunities that suit you best.",
+
+
+
+
+    "about-content":
+    "At Zenith Realty, we offer reliable and professional consultancy services with years of experience in Bursa’s real estate sector. Our mission is to meet our clients' real estate needs at the highest level of satisfaction, while our vision is to become a leading brand in the industry by providing innovative, reliable, and sustainable solutions. Our expert team in Bursa, each a professional in their field, serves you with a commitment to honesty and transparency. With the experience we've gained over the years, we closely follow all the intricacies and developments in the real estate sector to provide you with the most up-to-date and effective solutions. Zenith Realty aims to be more than just a mediator in your real estate investments; we strive to be a trustworthy companion on your journey. With our customer-oriented approach, we proudly stand by you at every stage of your real estate transactions, protecting your interests. Our extensive portfolio, carefully evaluated properties, and commitment to offering quality solutions make us a leading real estate consultancy service in Bursa. Whether you are looking to invest or find a home to live in, Zenith Realty provides safe and satisfaction-focused services by offering you the best options.",
+    "why-title": "Why Choose Us?",
+    "why-reliability": "Reliability",
+    "why-reliability-content":
+      "We offer reliable service with years of experience from our expert team.",
+    "why-quality": "Quality",
+    "why-quality-content":
+      "We always focus on providing the highest quality solutions.",
+    "why-satisfaction": "Customer Satisfaction",
+    "why-satisfaction-content":
+      "Customer satisfaction has always been the most important priority.",
+    "team-title": "Team",
+    "team-member1-name": "Halil İbrahim AYBEY",
+    "team-member1-role": "Real Estate Consultant",
+    "team-member2-name": "Hediye ÇELİK",
+    "team-member2-role": "Broker",
+    "team-member3-name": "Soner AYDIN",
+    "team-member3-role": "Real Estate Consultant",
+    "exploretext" : "Explore our portfolio ",
+    
+    "explore-treng": "Explore our porfolio and check out opportunities you won't want to miss ",
+    "contact-title": "Contact",
+    "contact-subtitle1": "Contact Us",
+    "contact-name": "Your Name",
+    "contact-email": "Email",
+    "contact-message": "Your Message",
+    "contact-submit": "Send",
+    "contact-address-title": "Address Information",
+    "copyr": "&copy; 2024 Ajans FLU. All Rights Reserved.",
+    "contact-address-content":
+      "Zenith Realty<br />Tepkar İş Merkezi Alaattinbey Mah.<br /> 626 Sk. No:12 Kat:2 Kapı No:7 <br />Nilüfer, Bursa<br />Phone: (0212) 123 45 67<br />Email: zenithrealty16@gmail.com",
+  },
+};
+
+function changeLanguage(language) {
+  const elements = document.querySelectorAll("[id]");
+  elements.forEach((element) => {
+    const key = element.id;
+    if (translations[language][key]) {
+      element.innerHTML = translations[language][key];
+    }
+  });
+}
